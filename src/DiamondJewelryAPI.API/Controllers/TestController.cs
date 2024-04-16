782ac12ce708b30b35e712c40f0a3abd4d5de7d6
varia <ltnhan2610@gmail.com>
2024-04-16 16:57:59 +0700

    modify test controller

--- src/DiamondJewelryAPI.API/Controllers/TestController.cs
+++ src/DiamondJewelryAPI.API/Controllers/TestController.cs
@@ -1,16 +1,14 @@
 using BuberDinner.Api.Controllers;
 
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DiamondJewelryAPI.API.Controllers;
 
-[AllowAnonymous]
 public class TestController : ApiController
 {
-    [HttpGet("[controller]")]
+    [HttpGet()]
     public IActionResult Test()
     {
-        return Problem();
+        return Ok();
     }
 }
